7d08e6b94ef56bfe72fed48808a4103ba9dc6bd2
wedontdozero <129505298+wedontdozero@users.noreply.github.com>
2025-04-07 16:36:17 +0700

    Personalize

--- bot.py
+++ bot.py
@@ -825,7 +825,7 @@ async def start(update, context): # 当用户输入/start时，返回文本
     else:
         update_language_status("English", chat_id=convo_id)
     message = (
-        f"Hello `{user.username}`! What shall we talk about today?\n\n"
+        f"Hello {user.first_name}! What shall we talk about today?\n\n"
     )
     if len(context.args) == 2 and context.args[1].startswith("sk-"):
         api_url = context.args[0]
